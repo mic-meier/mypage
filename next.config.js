eb410def2b54a1a1ed68cb3ac1f472c5bcf1bab4
ElMayo <mic.l.meier@gmail.com>
2020-11-29 10:05:48 +0100

    chore: removed withMDX from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
-
 module.exports = {
   images: {
     domains: ['images.unsplash.com'],
   },
-  withMDX: withMDX({
-    extension: /\.mdx?$/,
-  }),
 }
